53d177ec073d3001710e464d083e5bcf12f3fcb2
Matthias Ernst <m.ernst0702@gmail.com>
2023-03-01 17:27:11 +0100

    Changed Bitrate to 64kbps

--- Barotrauma/BarotraumaClient/ClientSource/Networking/Voip/VoipConfig.cs
+++ Barotrauma/BarotraumaClient/ClientSource/Networking/Voip/VoipConfig.cs
@@ -11,7 +11,7 @@ namespace Barotrauma.Networking
     static partial class VoipConfig
     {
         public const int FREQUENCY = 48000; //48Khz
-        public const int BITRATE = 16000; //16Kbps
+        public const int BITRATE = 64000; //64Kbps
         public const int BUFFER_SIZE = (8 * MAX_COMPRESSED_SIZE * FREQUENCY) / BITRATE; //20ms window
 
         public static OpusEncoder CreateEncoder()
